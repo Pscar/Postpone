f91fae4d637c9eb878931b9b3c12fea30ac69e4b
pscar <pangza115@hotmail.com>
2021-11-11 18:14:09 +0700

    +

--- backend/route/index.js
+++ backend/route/index.js
@@ -21,7 +21,7 @@ router.get("/user/getbyemail", UserController.GetUserByEmail);
 // //!API 105 - Edit User By ID
 router.put("/user/editbyid", UserController.EditUserByID);
 // //!API 106 - Edit User By Email
-router.post("/user/editbyemail", UserController.EditUserByEmail);
+router.put("/user/editbyemail", UserController.EditUserByEmail);
 // //!API 107 - Delete User By ID
 router.delete("/user/deletebyid", UserController.DeleteUserByID);
 // //!API 108 - Delete User By Email
@@ -49,7 +49,7 @@ router.get("/doctor/get", DoctorController.GetDoctorAll);
 //!API 303 - Get Doctor By ID
 router.get("/doctor/getbyid", DoctorController.GetDoctorByID);
 // !API 304 - Edit Doctor By ID
-router.post("/doctor/editbyid", DoctorController.EditDoctorID);
+router.put("/doctor/editbyid", DoctorController.EditDoctorID);
 // !API 305 - Delete Doctor By ID
 router.delete("/doctor/deletebyid", DoctorController.DeleteDoctorID)
 
@@ -62,7 +62,7 @@ router.get("/schedule/get", ScheduleDoctorController.GetScheduleDoctorAll);
 //!API 403 - Get ScheduleDoctor By ID
 router.get("/schedule/getbyid", ScheduleDoctorController.GetScheduleDoctorByID);
 // !API 404 - Edit ScheduleDoctor By ID
-router.post("/schedule/editbyid", ScheduleDoctorController.EditScheduleByID);
+router.put("/schedule/editbyid", ScheduleDoctorController.EditScheduleByID);
 // !API 405 - Delete ScheduleDoctor By ID
 router.delete("/schedule/deletebyid", ScheduleDoctorController.DeleteScheduleByID)
 module.exports = router;
